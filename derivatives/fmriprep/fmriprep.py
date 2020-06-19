9028f3e9d080b72d3f76b2b8a4933a4e0a506522
basile <basile.pinsard@gmail.com>
2020-06-19 15:18:34 -0400

    add more cpu

--- derivatives/fmriprep/fmriprep.py
+++ derivatives/fmriprep/fmriprep.py
@@ -10,8 +10,8 @@ script_dir = os.path.dirname(__file__)
 PYBIDS_CACHE_PATH = '.pybids_cache'
 SLURM_JOB_DIR = '.slurm'
 
-SMRIPREP_REQ = {'cpus': 4, 'mem_per_cpu': 4096, 'time':'24:00:00'}
-FMRIPREP_REQ = {'cpus': 4, 'mem_per_cpu': 4096, 'time':'36:00:00'}
+SMRIPREP_REQ = {'cpus': 8, 'mem_per_cpu': 4096, 'time':'24:00:00'}
+FMRIPREP_REQ = {'cpus': 8, 'mem_per_cpu': 4096, 'time':'36:00:00'}
 
 FMRIPREP_VERSION = "fmriprep-20.1.0"
 FMRIPREP_SINGULARITY_PATH = os.path.abspath(os.path.join(script_dir, f"../../containers/{FMRIPREP_VERSION}.simg"))
