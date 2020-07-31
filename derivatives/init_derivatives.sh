e4680e6b01124b267eb4e4239a7edf64d5d573af
basile <basile.pinsard@gmail.com>
2020-07-31 17:24:15 -0400

    add default gitattributes for derivatives

--- derivatives/init_derivatives.sh
+++ derivatives/init_derivatives.sh
@@ -15,18 +15,27 @@ derivative_path=$bids_path/derivatives/$derivative
 ds_name=$(basename $bids_path)
 
 
-datalad create -d $bids_path $derivative_path
 if [ -n "$remote" ] ; then
+  datalad install -s $remote $derivative_path
   # configure git remote
-  datalad siblings -s origin -d $derivative_path --url $remote add
+  #datalad siblings -s origin -d $derivative_path --url $remote add
+
+  # set default gitattributes to avoid annexing small files/metas
+  cp ${BASH_SOURCE%/*}/.gitattributes_default $derivative_path/.gitattributes
+
+  pushd $derivative_path
+
+  datalad save -m 'add default gitattributes' .gitattributes
 
   # configure s3 bucket
-  pushd $derivative_path
   bucket_name="cneuromod."${ds_name}".derivatives."$derivative
   init_remote_s3 $bucket_name
   git annex wanted $bucket_name "not metadata=distribution-restrictions=*"
+
   popd
 
   #push the derivatives to git
   datalad publish -d $derivative_path --to origin
+else ;
+  datalad create -d $bids_path $derivative_path
 fi
