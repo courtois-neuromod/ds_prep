5cc026230a0cd475664e525481df5be3f6d006dd
basile <basile.pinsard@gmail.com>
2021-06-11 17:01:25 -0400

    attempt to recover lost bk2, wip

--- stimuli/games/log2events.py
+++ stimuli/games/log2events.py
@@ -15,6 +15,7 @@ def logs2event_files(in_files, out_file_tpl):
     run = 0
     TTL = None
     last_rep_last_run = 'aaa'
+    rep_log = None
     for in_file in in_files:
         log = np.loadtxt(
             in_file,
@@ -25,8 +26,12 @@ def logs2event_files(in_files, out_file_tpl):
             converters={0: float, 1: lambda x: x.strip(), 2: lambda x: x.strip()},
         )
         for e in log:
+            if not rep_log is None:
+                rep_log.append(e)
+
             if record_tag in e[2]:
                 record = "/".join(e[2].split(" ")[-1].split("/")[-4:])
+                rep_log = []
             elif e[2] == TTL_tag:
                 if not TTL:
                     run += 1  # only increment if the previous scan was not aborted
@@ -55,6 +60,8 @@ def logs2event_files(in_files, out_file_tpl):
                                 None,
                             )
                         )
+                        keypresses, rewards = extract_keypress_rewards(rep_log)
+                        print(keypresses, rewards)
                     else:
                         repetition[-1].append(
                             (
@@ -73,7 +80,6 @@ def logs2event_files(in_files, out_file_tpl):
                 TTL = None
                 if len(repetition): # only if TTL was received
                     del repetition[-1]
-
         TTL = None  # reset the TTL
     run = 0
     for reps in repetition:
@@ -85,3 +91,22 @@ def logs2event_files(in_files, out_file_tpl):
             reps, columns=["trial_type", "onset", "duration", "duration_bk2", "level", "stim_file"]
         )
         df.to_csv(out_file, sep="\t", index=False)
+
+
+
+def extract_keypress_rewards(rep_log):
+    keypresses = {}
+    rewards = {}
+    for e in rep_log:
+        if 'level step:' in e[2]:
+            ref = (e[0], int(e[2].split(' ')[2]))
+        elif 'Keypress' in e[2]:
+            key = e[2].split(' ')[1]
+            if key in 'rludyab':
+                keypress_step = ref[1] + int(np.ceil((e[0]-ref[0])/60.))
+                keypresses[keypress_step] = key
+        elif 'Reward' in e[2]:
+            reward = float(e[2].split(' ')[1])
+            reward_step = ref[1] + int(np.ceil((e[0]-ref[0])/60.))
+            rewards[reward_step] = reward
+    return keypresses, rewards
