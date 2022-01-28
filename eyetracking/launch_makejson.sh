3eea00e4e5269a9506001364c48cb153d3b9c38e
MarieStLaurent <marie.stl@gmail.com>
2022-01-28 17:32:32 -0500

    elm paths

--- eyetracking/launch_makejson.sh
+++ eyetracking/launch_makejson.sh
@@ -2,7 +2,8 @@
 
 #module load python/3.7 #?? on elm?
 
-source /home/mariestl/my_envs/pupil_venv/bin/activate
+#source /home/mariestl/my_envs/pupil_venv/bin/activate
+source /data/neuromod/virtualenvs/eyetracking/bin/activate
 
 CODEDIR="/home/mariestl/cneuromod/ds_prep/eyetracking"
 DATADIR="/data/neuromod/DATA/fmri_tmp/things/sourcedata"
