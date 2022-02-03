78ecf4649340c449366d1faea0499924b3544eb5
Marie St-Laurent <mariestl@elm.criugm.qc.ca>
2022-02-03 11:02:56 -0500

    mkjson

--- eyetracking/launch_makejson.sh
+++ eyetracking/launch_makejson.sh
@@ -5,6 +5,8 @@
 #source /home/mariestl/my_envs/pupil_venv/bin/activate
 source /data/neuromod/virtualenvs/eyetracking/bin/activate
 
+export LD_LIBRARY_PATH=/data/neuromod/virtualenvs/eyetracking/lib
+
 CODEDIR="/home/mariestl/cneuromod/ds_prep/eyetracking"
 DATADIR="/data/neuromod/DATA/fmri_tmp/things/sourcedata"
 OUTDIR="/home/mariestl/cneuromod/THINGS/Eye-tracking/offline_calibration"
