c36289b9c2f9d69d0b3959bc2eba400d7e7e1a7b
bpinsard <basile.pinsard@gmail.com>
2022-04-13 11:04:29 -0400

    allows merging fmriprep results on partially ran dataset

--- derivatives/fmriprep/merge_fmriprep.sh
+++ derivatives/fmriprep/merge_fmriprep.sh
@@ -3,15 +3,23 @@ current_branch=$(git symbolic-ref --short HEAD)
 ria_store=ria-beluga
 
 datalad update -s $ria_store
-for b in $(git branch -la | grep 'fmriprep_' ) ; do
+for b in $(git branch -la | grep "remotes/$ria_store/fmriprep_" ) ; do
+    sub_ses_tmp=${b##*/}
+    echo 'editing branch' $sub_ses_tmp
+    sub_ses_tmp=${sub_ses_tmp#fmriprep_study-*_}
+    sub_ses_tmp=${sub_ses_tmp%.job}
+    sub_ses=${sub_ses_tmp/_/\/}
+    if [ -d $sub_ses ] ; then continue ; fi
     git checkout ${b##*/} ;
-    b2=${b%.job};
-    git mv sub-*.html sub-${b2#*_sub-}.html ;
+    git mv sub-??.html ${sub_ses_tmp}.html ;
     git add sourcedata/templateflow
     git commit -m 'append session entity to report to avoid name collision' ;
+    git push $ria_store $b
 done
 
 git checkout $current_branch
 for b in $(git branch -l | grep 'fmriprep_' ) ; do
-    git merge -Xours --no-edit $b ;
+    echo "merging" $b
+    git merge -Xours --no-edit $b
+    git branch -d $b
 done
