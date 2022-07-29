22000702adafc09c625b08fe58a65f5f2b6ddd34
bpinsard <basile.pinsard@gmail.com>
2022-07-29 15:02:37 -0400

    mriqc submit script, small fix to fmriprep one

--- derivatives/mriqc/bootstrap.sh
+++ derivatives/mriqc/bootstrap.sh
@@ -3,24 +3,18 @@
 #source_ds=$1
 ria_store=$1
 ds_name=$2
-source_ds=${ria_store}#~${ds_name}.raw
-#ria_store=ria+file:///lustre03/project/rrg-pbellec/ria-beluga
+source_ds=${ria_store}#~cneuromod.${ds_name}.raw
 ria_name=${ria_store##*/}
 
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-datalad install -d . -s ${ria_store}#~templateflow ./sourcedata/templateflow
-
 datalad install -d . -s ${ria_store}#~repronim/containers ./containers
 git -C containers remote rename origin ria-beluga #rename to ria-beluga to be more explicit
-datalad run -m "Freeze fmriprep container version" containers/scripts/freeze_versions bids-fmriprep=20.2.5
+datalad run -m "Freeze mriqc container version" containers/scripts/freeze_versions bids-mriqc=22.0.1
 datalad push -d containers --to ria-beluga # push changes to the RIA for the freeze commit to exists there
 
-datalad install -d . -s ${ria_store}#~cneuromod.anat.smriprep sourcedata/smriprep
-
 mkdir code
-cp $SCRIPT_DIR/freesurfer.license code/
-cp $SCRIPT_DIR/../.gitattributes_default .gitattributes
+cp $SCRIPT_DIR/.gitattributes_default .gitattributes
 echo "workdir/" >> .gitignore
 echo 'code/*.{out,err}' >> .gitignore
 
@@ -28,6 +22,6 @@ datalad save -m 'setup dataset gitattribute gitignore and install freesurfer lic
 
 datalad install -d . -s $source_ds sourcedata/$ds_name
 
-datalad create-sibling-ria  --alias $ds_name.fmriprep -s $ria_name $ria_store --shared 640
+datalad create-sibling-ria  --alias cneuromod.$ds_name.mriqc -s $ria_name $ria_store --shared 640
 datalad push --to $ria_name
 git -C $(git remote get-url $ria_name) symbolic-ref HEAD refs/heads/$(git branch --show-current)
