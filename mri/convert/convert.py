76a7a0cf76316227eaffb6195878286151563857
test <basile.pinsard@gmail.com>
2022-11-04 14:56:45 -0400

    fix convert script fixes to only consider files create by the con current conversion

--- mri/convert/convert.py
+++ mri/convert/convert.py
@@ -49,42 +49,47 @@ def single_session_job(input_file, output_datalad, ria_storage_remote):
     remote_path = pathlib.Path(output_datalad.replace('ria+file://', '').replace('#~', '/alias/').split('@')[0])
     lock_path = remote_path / '.datalad_lock'
     file_lock = FileLock(lock_path)
-    
-    with tempfile.TemporaryDirectory() as tmpdirname:
-        with file_lock:
-            ds = datalad.api.install(path=tmpdirname, source=output_datalad)
 
-        # enable the ria storage remote
-        ds.repo.enable_remote(ria_storage_remote)
-        # checkout a new branch
-        ds.repo.checkout(session_name, options=['-b'])
-        # this clone will be flush, better say it's already dead.
-        ds.repo.set_remote_dead('here')
-        
-        heudiconv_params = dict(
-            files=[input_file],
-            outdir=tmpdirname,
-            bids_options=[],
-            datalad=True,
-            heuristic=str(HEURISTICS_PATH)
-        )
-        print(heudiconv_params)
-        heudiconv_workflow(**heudiconv_params)
+    try:
+        with tempfile.TemporaryDirectory() as tmpdirname:
+            with file_lock:
+                ds = datalad.api.install(path=tmpdirname, source=output_datalad)
 
-        fix_fmap_phase(ds)
-        fix_complex_events(ds)
-        fill_intended_for(ds.pathobj)
-        ds.save(message='fill IntendedFor')
-        
-        with file_lock:
-            print('pushing')
-            ds.push(to='origin', data='anything')
-        ds.repo.call_annex(['unused'])
-        ds.repo.call_annex(['dropunused', '--force', 'all'])
-        ds.drop('./.heudiconv/', reckless='kill', recursive=True)
-        ds.drop('.', reckless='kill', recursive=True)
+            # enable the ria storage remote
+            ds.repo.enable_remote(ria_storage_remote)
+            # checkout a new branch
+            ds.repo.checkout(session_name, options=['-b'])
+            # this clone will be flush, better say it's already dead.
+            ds.repo.set_remote_dead('here')
 
-    print(f"processed {input_file}")
+            heudiconv_params = dict(
+                files=[input_file],
+                outdir=tmpdirname,
+                bids_options=[],
+                datalad=True,
+                heuristic=str(HEURISTICS_PATH)
+            )
+            print(heudiconv_params)
+            heudiconv_workflow(**heudiconv_params)
+
+            fix_fmap_phase(ds)
+            fix_complex_events(ds)
+            fill_intended_for(ds.pathobj)
+            ds.save(message='fill IntendedFor')
+
+            with file_lock:
+                print('pushing')
+                ds.push(to='origin', data='anything')
+            ds.repo.call_annex(['unused'])
+            ds.repo.call_annex(['dropunused', '--force', 'all'])
+            ds.drop('./.heudiconv/', reckless='kill', recursive=True)
+            ds.drop('.', reckless='kill', recursive=True)
+        print(f"processed {input_file}")
+    except Exception as e:
+        print(f"An error occur processing {input_file}")
+        print(e)
+        import traceback
+        print(traceback.format_exc())
 
 def fix_fmap_phase(ds):
     #### fix fmap phase data (sbref series will contain both and heudiconv auto name it)
@@ -112,14 +117,16 @@ def fix_fmap_phase(ds):
 
 def fix_complex_events(ds):
     # remove phase event files.
-    phase_events = 'sub-*/ses-*/func/*_part-phase*_events.tsv'
-    if not list(ds.pathobj.glob(phase_events)):
-        return
-    ds.repo.remove(phase_events)
-    # remove part-mag from remaining event files 
-    for f in ds.pathobj.glob('sub-*/ses-*/func/*_part-mag*_events.tsv'):
-        ds.repo.call_git(['mv', str(f), str(f).replace('_part-mag','')])
-    ds.save(message='fix complex event files')
+    new_files = [(ds.pathobj / nf) for nf in ds.repo.call_git(['show','--name-only','HEAD~1','--format=oneline']).split('\n')[1:]]
+    phase_events = [f for f in ds.pathobj.glob('sub-*/ses-*/func/*_part-phase*_events.tsv') if f in new_files]
+    if list(phase_events):
+        ds.repo.remove(phase_events)
+    # remove part-mag from remaining event files
+    mag_events = [f for f in ds.pathobj.glob('sub-*/ses-*/func/*_part-mag*_events.tsv') if f in new_files]
+    if len(mag_events):
+        for f in mag_events:
+            ds.repo.call_git(['mv', str(f), str(f).replace('_part-mag','')])
+        ds.save(message='fix complex event files')
     
 def main():
 
