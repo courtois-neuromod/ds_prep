8157c2c2791845e3abe1805d60fbbebb982bc5c0
MarieStLaurent <marie.stl@gmail.com>
2023-05-20 18:20:15 -0400

    triplets cleanup first step

--- eyetracking/bids_scripts/launch_tripletsclean.sh
+++ eyetracking/bids_scripts/launch_tripletsclean.sh
@@ -4,9 +4,10 @@ source /data/neuromod/virtualenvs/eyetracking/bin/activate
 DATADIR="/unf/eyetracker/neuromod/triplets/sourcedata"
 RUNDIR="/home/mariestl/cneuromod/ds_prep/eyetracking"
 OUTDIR="/home/mariestl/cneuromod/eyetracking/triplets"
+PHASE="${1}" # A, B
 
 python -m triplets_cleanup \
       --in_path="${DATADIR}" \
       --run_dir="${RUNDIR}" \
-      --phase="A" \
+      --phase="${PHASE}" \
       --out_path="${OUTDIR}"
