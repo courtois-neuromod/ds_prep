9093a088ea7222ada4c52db4b274b9ed16c98023
MarieStLaurent <marie.stl@gmail.com>
2023-05-26 16:21:37 -0400

    eyetracking cleanup step 1

--- eyetracking/bids_scripts/launch_etprep_step1.sh
+++ eyetracking/bids_scripts/launch_etprep_step1.sh
@@ -4,7 +4,7 @@ source /data/neuromod/virtualenvs/eyetracking/bin/activate
 TASK="${1}" # emotionsvideos, floc, friends, mario, mario3, mariostars, retino, things, triplets
 DATADIR="/unf/eyetracker/neuromod/${TASK}/sourcedata"
 RUNDIR="/home/mariestl/cneuromod/ds_prep/eyetracking"
-OUTDIR="/home/mariestl/cneuromod/eyetracking/${TASK}"
+OUTDIR="/data/neuromod/projects/eyetracking_bids/${TASK}"
 
 python -m et_prep_step1 \
       --in_path="${DATADIR}" \
