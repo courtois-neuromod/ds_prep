4f08622b82684368da27078c5cde08cefd3c27ee
MarieStLaurent <marie.stl@gmail.com>
2023-06-01 22:31:23 -0400

    eyetracking cleanup step 2

--- eyetracking/bids_scripts/et_prep_step1.py
+++ eyetracking/bids_scripts/et_prep_step1.py
@@ -49,18 +49,15 @@ def compile_file_list(in_path):
 
     for ses_path in ses_list:
         [sub_num, ses_num] = ses_path.split('/')[-2:]
-        events_list = sorted(glob.glob(f'{ses_path}/*task*events.tsv'))
-        for event in events_list:
-            ev_file = os.path.basename(event)
 
-            try:
-                if task_root in ['retino', 'floc']:
-                    skip_run_num = True
-                    [sub, ses, fnum, task_type, appendix] = ev_file.split('_')
-                    run_num = run2task_mapping[task_root][task_type]
-                else:
-                    skip_run_num = False
-                    [sub, ses, fnum, task_type, run_num, appendix] = ev_file.split('_')
+        if task_root == 'friends':
+            epfile_list = sorted(glob.glob(f'{ses_path}/sub-*.pupil/task-friends-*/000'))
+
+            for epfile in epfile_list:
+                f1, f2 = epfile.split('/')[-3:-1]
+                t1, t2, run_num = f2.split('-')
+                task_type = f'{t1}-{t2}'
+                sub, ses, fnum = f1.split('.')[0].split('_')
 
                 if sub in ['sub-01', 'sub-02', 'sub-03', 'sub-04', 'sub-05', 'sub-06']:
                     assert sub == sub_num
@@ -68,23 +65,54 @@ def compile_file_list(in_path):
 
                     has_log = len(glob.glob(f'{ses_path}/{sub_num}_{ses_num}_{fnum}.log')) == 1
 
-                    if skip_run_num:
-                        pupil_path = f'{ses_path}/{sub_num}_{ses_num}_{fnum}.pupil/{task_type}/000'
-                    else:
-                        pupil_path = f'{ses_path}/{sub_num}_{ses_num}_{fnum}.pupil/{task_type}_{run_num}/000'
-
-                    list_pupil = glob.glob(f'{pupil_path}/pupil.pldata')
+                    list_pupil = glob.glob(f'{epfile}/pupil.pldata')
                     has_pupil = len(list_pupil) == 1
                     if has_pupil:
                         pupil_file_paths.append((os.path.dirname(list_pupil[0]), (sub, ses, run_num, task_type, fnum)))
 
-                    has_eyemv = len(glob.glob(f'{pupil_path}/eye0.mp4')) == 1
-                    has_gaze = len(glob.glob(f'{pupil_path}/gaze.pldata')) == 1
+                    has_eyemv = len(glob.glob(f'{epfile}/eye0.mp4')) == 1
+                    has_gaze = len(glob.glob(f'{epfile}/gaze.pldata')) == 1
 
                     run_data = [sub_num, ses_num, run_num, task_type, fnum, has_pupil, has_gaze, has_eyemv, has_log]
                     df_files = pd.concat([df_files, pd.DataFrame(np.array(run_data).reshape(1, -1), columns=df_files.columns)], ignore_index=True)
-            except:
-                print(f'cannot process {ev_file}')
+
+        else:
+            events_list = sorted(glob.glob(f'{ses_path}/*task*events.tsv'))
+            for event in events_list:
+                ev_file = os.path.basename(event)
+
+                try:
+                    if task_root in ['retino', 'floc']:
+                        skip_run_num = True
+                        [sub, ses, fnum, task_type, appendix] = ev_file.split('_')
+                        run_num = run2task_mapping[task_root][task_type]
+                    else:
+                        skip_run_num = False
+                        [sub, ses, fnum, task_type, run_num, appendix] = ev_file.split('_')
+
+                    if sub in ['sub-01', 'sub-02', 'sub-03', 'sub-04', 'sub-05', 'sub-06']:
+                        assert sub == sub_num
+                        assert ses_num == ses
+
+                        has_log = len(glob.glob(f'{ses_path}/{sub_num}_{ses_num}_{fnum}.log')) == 1
+
+                        if skip_run_num:
+                            pupil_path = f'{ses_path}/{sub_num}_{ses_num}_{fnum}.pupil/{task_type}/000'
+                        else:
+                            pupil_path = f'{ses_path}/{sub_num}_{ses_num}_{fnum}.pupil/{task_type}_{run_num}/000'
+
+                        list_pupil = glob.glob(f'{pupil_path}/pupil.pldata')
+                        has_pupil = len(list_pupil) == 1
+                        if has_pupil:
+                            pupil_file_paths.append((os.path.dirname(list_pupil[0]), (sub, ses, run_num, task_type, fnum)))
+
+                        has_eyemv = len(glob.glob(f'{pupil_path}/eye0.mp4')) == 1
+                        has_gaze = len(glob.glob(f'{pupil_path}/gaze.pldata')) == 1
+
+                        run_data = [sub_num, ses_num, run_num, task_type, fnum, has_pupil, has_gaze, has_eyemv, has_log]
+                        df_files = pd.concat([df_files, pd.DataFrame(np.array(run_data).reshape(1, -1), columns=df_files.columns)], ignore_index=True)
+                except:
+                    print(f'cannot process {ev_file}')
 
     return df_files, pupil_file_paths
 
@@ -147,6 +175,8 @@ def export_and_plot(pupil_path, in_path, out_path):
         try:
             if task_root in ['retino', 'floc']:
                 ev_path = f'{in_path}/{sub}/{ses}/{sub}_{ses}_{fnum}_{task}_events.tsv'
+            elif task_root == 'friends':
+                run_dur = 770
             else:
                 ev_path = f'{in_path}/{sub}/{ses}/{sub}_{ses}_{fnum}_{task}_{run}_events.tsv'
             ev_lasttrial = pd.read_csv(ev_path, sep='\t', header=0).iloc[-1]
