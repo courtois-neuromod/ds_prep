f3cda468b785559f6b243c55a1dbdb9736d2bb42
MarieStLaurent <marie.stl@gmail.com>
2024-01-09 16:54:35 -0500

    step2

--- eyetracking/bids_scripts/launch_things_driftCor.sh
+++
@@ -1,9 +0,0 @@
-
-source /data/neuromod/virtualenvs/eyetracking/bin/activate
-# run on elm...
-DATADIR="/unf/eyetracker/neuromod/things/sourcedata"
-OUTDIR="/data/neuromod/projects/eyetracking_bids/things"
-
-python -m things_driftCor \
-  --in_path="${DATADIR}" \
-  --out_path="${OUTDIR}"
