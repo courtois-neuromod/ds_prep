b4b2ecb6f3f07820d4f8d88bf56492871e4d4c86
test <basile.pinsard@gmail.com>
2024-01-17 11:05:02 -0500

    skip merge edit if already merged

--- mri/convert/merge_heudiconv.sh
+++ mri/convert/merge_heudiconv.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-
 remote=${1:-ria-sequoia}
 for b in $(git branch -a | grep -E "remotes/$remote/p0._[a-z0-9]+[0-9]{2,3}") ; do
-    git merge --no-edit -X ours ${b#remotes/}
+    echo $b
+    if [ "$(git merge --no-edit -X ours ${b#remotes/})" = 'Already up to date.' ] ;then
+	continue;
+    fi
     git rm .heudiconv
     GIT_EDITOR=/bin/true git commit --amend
 done
