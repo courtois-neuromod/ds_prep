7e7c682d482336bfb44aa94e9103ca05ec6fe368
MarieStLaurent <marie.stl@gmail.com>
2025-10-27 10:29:35 -0400

    tsnr avgmap script

--- derivatives/tsnr/avg_tsnr_maps.py
+++ derivatives/tsnr/avg_tsnr_maps.py
@@ -14,13 +14,13 @@ def main(ds_name, ds_path):
         sub_num = os.path.basename(sub_path)
 
         sub_list_MNI = sorted(glob.glob(f"{ds_path}/{sub_num}/ses*/func/*MNI152NLin2009cAsym*stat-tsnr*nii.gz"))
-        sub_mean_MNI = mean_img(sub_list_MNI)
+        sub_mean_MNI = mean_img(sub_list_MNI, copy_header=True)
         sub_mean_MNI.to_filename(
             f"{ds_path}/{sub_num}/{sub_num}_task-{ds_name}_space-MNI152NLin2009cAsym_stat-avgtsnr_statmap.nii.gz"
         )
 
         sub_list_t1w = sorted(glob.glob(f"{ds_path}/{sub_num}/ses*/func/*T1w*stat-tsnr*nii.gz"))
-        sub_mean_t1w = mean_img(sub_list_t1w)
+        sub_mean_t1w = mean_img(sub_list_t1w, copy_header=True)
         sub_mean_t1w.to_filename(
             f"{ds_path}/{sub_num}/{sub_num}_task-{ds_name}_space-T1w_stat-avgtsnr_statmap.nii.gz"
         )
