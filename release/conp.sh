2729612384a47b88e0f5260c6221f0cac40ff853
test <basile.pinsard@gmail.com>
2025-10-28 15:20:13 -0400

    add other CONP subjects

--- release/conp.sh
+++ release/conp.sh
@@ -7,12 +7,12 @@ ssh -fN -L  4722:sftp.conp.ca:6806 bpinsard@login.acelab.ca
 datalad create-sibling-ria --existing skip  -r -R 2 -s conp-ria ria+ssh://cneuromod@localhost:4722/data/proftpd/users/cneuromod/ria-conp --shared 0644
 
 # apply filters for the subset of data to be pushed to conp for datasets containing subject data
-git submodule foreach --recursive bash -c "git-annex groupwanted conp 'not metadata=distribution-restrictions=* and include=sub-0[135]/** and exclude=sub-0[246]/**' || true"
+git submodule foreach --recursive bash -c "git-annex groupwanted conp 'not metadata=distribution-restrictions=* and include=sub-0[12356]/** and exclude=sub-0[4]/**' || true"
 git submodule foreach --recursive bash -c "git-annex group conp-ria-storage conp || true"
 git submodule foreach --recursive bash -c "git-annex wanted conp-ria-storage groupwanted || true"
 
 # create https remote endpoint
-git submodule foreach --recursive bash -c 'git annex initremote conp-ria-storage-http  --sameas=conp-ria-storage  type=httpalso url=https://sftp.conp.ca/users/cneuromod/ria-conp/$(datalad configuration get datalad.dataset.id | sed "s|.|&/|3")/annex/objects/ cost=50 || true'
+git submodule foreach --recursive bash -c 'git annex initremote conp-ria-storage-http  --sameas=conp-ria-storage  type=httpalso url=https://sftp.conp.ca/users/cneuromod/ria-conp/$(datalad configuration get datalad.dataset.id | sed "s|.|&/|3")/annex/objects/ cost=50 autoenable=true || true'
 
 # remove autoenable to ria-ssh endpoint
 git submodule foreach --recursive git-annex enableremote conp-ria-storage autoenable=false
