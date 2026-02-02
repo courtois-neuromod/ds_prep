da7670b1a4e2bc47950026d1bafc33182e40e0f1
Marie-Eve Picard <marie-eve.picard.2@umontreal.ca>
2026-02-02 18:51:01 -0500

    Add support for multi-echo data

--- derivatives/tsnr/make_tsnr_maps.py
+++ derivatives/tsnr/make_tsnr_maps.py
@@ -12,19 +12,28 @@ from nipype.algorithms.confounds import TSNR
 @click.argument('ds_name', type=str)
 @click.argument('ds_path', type=click.Path())
 @click.argument('output_filepath', type=click.Path())
-def main(ds_name, ds_path, output_filepath):
-    tsnr_maps(ds_name, ds_path, output_filepath)
+@click.option('--echo', type=str, default=None, help='Specify the echo value to compute tSNR maps from fMRIPrep outputs')
+@click.option('--me', is_flag=True, help='Flag to specify if working with tedana outputs. If specified, will compute tSNR maps for optcom and denoised images')
+def main(ds_name, ds_path, output_filepath, echo, me):
+    tsnr_maps(ds_name, ds_path, output_filepath, echo, me)
 
-def tsnr_maps(ds_name, ds_path, output_filepath):
+def tsnr_maps(ds_name, ds_path, output_filepath, echo=None, me=False):
     logger = logging.getLogger(__name__)
     logger.info(f'generating tsnr maps from fmriprep data for the {ds_name} dataset')
     logger.info(f"loading BIDS: {ds_name}")
 
     layout = bids.BIDSLayout(ds_path, validate=False)
-    bolds = layout.get(suffix='bold', extension='.nii.gz', desc='preproc')
+    if me:
+        # Get optimally combined and denoised images
+        bolds_optcom = layout.get(suffix='bold', extension='.nii.gz', desc='optcom')
+        bolds_denoised = layout.get(suffix='bold', extension='.nii.gz', desc='denoised')
+        bolds = [*bolds_optcom, *bolds_denoised]
+    else:
+        bolds = layout.get(suffix='bold', extension='.nii.gz', desc='preproc', echo=echo)
 
     for bold in bolds:
-        tsnr_path = bold.path.replace(ds_path,output_filepath).replace('_part-mag','').replace('desc-preproc','stat-tsnr').replace('bold','statmap')
+        tsnr_path = bold.path.replace(ds_path,output_filepath).replace('_part-mag','').replace(f'desc-{bold.get_entities()["desc"]}',f'stat-tsnr_desc-{bold.get_entities()["desc"]}').replace('bold','statmap')
+
         Path(tsnr_path).parent.mkdir(parents=True, exist_ok=True)
         if not Path(tsnr_path).exists():
             try:
