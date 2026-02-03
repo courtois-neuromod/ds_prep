9211759e808f817554fe9f07d31f6b53cb00ece8
Marie-Eve Picard <marie-eve.picard.2@umontreal.ca>
2026-02-02 20:43:52 -0500

    Add tsnr to T1w resampling

---
+++ derivatives/tsnr/resample_tsnr_maps.py
@@ -0,0 +1,89 @@
+import os
+import logging
+from pathlib import Path
+
+import bids
+import click
+
+from ants import image_read, image_write
+from ants.registration import apply_transforms
+
+@click.command()
+@click.argument('ds_name', type=str)
+@click.argument('tsnr_path', type=click.Path())
+@click.argument('fmriprep_path', type=click.Path())
+def main(ds_name, tsnr_path, fmriprep_path):
+    resample_to_T1w(ds_name, tsnr_path, fmriprep_path)
+
+def resample_to_T1w(ds_name, tsnr_path, fmriprep_path):
+    logger = logging.getLogger(__name__)
+    logger.info(f'generating tsnr maps from {Path(tsnr_path).name.split('.')[-1]} data for the {ds_name} dataset')
+    logger.info(f"loading tSNR data: {tsnr_path}")
+    layout_tsnr = bids.BIDSLayout(tsnr_path, validate=False, is_derivative=True)
+
+    logger.info(f"loading fMRIPrep data: {fmriprep_path}")
+    layout_fmriprep = bids.BIDSLayout(fmriprep_path, validate=False, is_derivative=True)
+    
+    tsnr_maps = [f for f in layout_tsnr.get(suffix='statmap') if 'stat-tsnr' in f.filename and 'space-T1w' not in f.filename]
+
+    for tsnr_map in tsnr_maps:
+        entities = tsnr_map.get_entities()
+        out_file = tsnr_map.path.replace(f'run-{entities["run"]}', f'run-{entities["run"]}_space-T1w')
+        
+        Path(out_file).parent.mkdir(parents=True, exist_ok=True)
+        if not Path(out_file).exists():
+            # Get reference images from fMRIPrep directory
+            t1w = layout_fmriprep.get(
+                subject=entities['subject'],
+                session=entities['session'],
+                run=entities['run'],
+                space='T1w',
+                suffix='boldref',
+                extension='nii.gz'
+            )
+            # Validate input
+            if len(t1w) == 0:
+                raise IOError(f"No T1w file associated with {tsnr_map.filename}")
+            elif len(t1w) > 1:
+                raise IOError(f"More than one T1w file associated with {tsnr_map.filename}")
+            else:
+                t1w = t1w[0]
+            
+            # Get transform from fMRIPrep directory
+            transform = layout_fmriprep.get(
+                subject=entities['subject'],
+                session=entities['session'],
+                run=entities['run'],
+                desc='coreg',
+                suffix='xfm',
+                extension='txt'
+            )
+            # Validate input
+            if len(transform) == 0:
+                raise IOError(f"No transform file associated with {tsnr_map.filename}")
+            elif len(transform) > 1:
+                raise IOError(f"More than one transform file associated with {tsnr_map.filename}")
+            else:
+                transform = transform[0]
+            
+            try:
+                # Resample tsnr map using ants
+                tsnr_T1w = apply_transforms(
+                    fixed=image_read(t1w.path),
+                    moving=image_read(tsnr_map.path),
+                    transformlist=[transform.path],
+                    interpolation='linear'
+                )
+                # Save resampled map
+                image_write(
+                    tsnr_T1w, 
+                    out_file
+                )
+            except:
+                logger.info(f"Could not resample {os.path.basename(tsnr_map.path)}")
+
+if __name__ == '__main__':
+    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    logging.basicConfig(level=logging.INFO, format=log_fmt)
+
+    main()
